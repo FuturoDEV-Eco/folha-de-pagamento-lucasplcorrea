652fa8d7f50dc3a2b585cd70d75e734f7273a2a0
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-07-12 11:03:11 -0300

    Ajuste de Percentuais de acordo com a tabela 2024

--- src/calculo_imposto_renda.js
+++ src/calculo_imposto_renda.js
@@ -1,20 +1,19 @@
 function calcularImpostoRenda(salarioBruto) {
-    let imposto;
-  
-    if (salarioBruto <= 2112.00) {
-      imposto = 0;
-    } else if (salarioBruto <= 2826.65) {
-      imposto = (salarioBruto - 2112.00) * 0.075;
-    } else if (salarioBruto <= 3751.05) {
-      imposto = (salarioBruto - 2826.65) * 0.15 + (2826.65 - 2112.00) * 0.075;
-    } else if (salarioBruto <= 4664.68) {
-      imposto = (salarioBruto - 3751.05) * 0.225 + (3751.05 - 2826.65) * 0.15 + (2826.65 - 2112.00) * 0.075;
-    } else {
-      imposto = (salarioBruto - 4664.68) * 0.275 + (4664.68 - 3751.05) * 0.225 + (3751.05 - 2826.65) * 0.15 + (2826.65 - 2112.00) * 0.075;
-    }
-  
-    return imposto;
+  let imposto;
+
+  if (salarioBruto <= 2259.20) {
+    imposto = 0;
+  } else if (salarioBruto <= 2826.65) {
+    imposto = salarioBruto * 0.075 - 169.44;
+  } else if (salarioBruto <= 3751.05) {
+    imposto = salarioBruto * 0.15 - 381.44;
+  } else if (salarioBruto <= 4664.68) {
+    imposto = salarioBruto * 0.225 - 662.77;
+  } else {
+    imposto = salarioBruto * 0.275 - 896.00;
   }
-  
-  module.exports = calcularImpostoRenda;
-  
+
+  return imposto;
+}
+
+module.exports = calcularImpostoRenda;
