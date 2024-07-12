fbfd37107cc09be55e3071e3ba0248114cfc0dfa
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-07-12 11:14:33 -0300

    Correção de calculo

--- src/calculo_inss.js
+++ src/calculo_inss.js
@@ -3,11 +3,11 @@ function calcularINSS(salarioBruto) {
 
   if (salarioBruto <= 1412.00) {
     inss = salarioBruto * 0.075;
-  } else if (salarioBruto <= 2666.68) {
+  } else if (salarioBruto > 1412.00 && salarioBruto <= 2666.68) {
     inss = salarioBruto * 0.09 - 21.18;
-  } else if (salarioBruto <= 4000.03) {
+  } else if (salarioBruto > 2666.68 && salarioBruto <= 4000.03) {
     inss = salarioBruto * 0.12 - 101.18;
-  } else if (salarioBruto <= 7786.02) {
+  } else if (salarioBruto > 4000.03 && salarioBruto <= 7786.02) {
     inss = salarioBruto * 0.14 - 181.18;
   } else {
     inss = 7786.02 * 0.14 - 181.18;
